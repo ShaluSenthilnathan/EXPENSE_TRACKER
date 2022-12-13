0d1bc99e3bf80eaa9fc278f45df2fa000ea1622e
ShaluSenthilnathan <91516570+ShaluSenthilnathan@users.noreply.github.com>
2022-12-13 06:55:53 +0530

    Update views.py

--- EXPENSE_TRACKER_DJANGO_FILES/USERACCOUNTS/views.py
+++ EXPENSE_TRACKER_DJANGO_FILES/USERACCOUNTS/views.py
@@ -65,19 +65,19 @@ def logout(request):
 def enteroreditdata(request):
     return render(request,'enteroreditdata.html')
 
-#Going wrong - data input from forms to database 
+#Going wrong...... 
 def userinfo(request):
     if request.method == 'POST':
-        userid = request.POST['userid']
-        usernname = request.POST['username']
-        gender = request.POST['gender']
-        age = request.POST['age']
-        email = request.POST['email']
-        phone_no = request.POST['phone_no']
-        working_or_not = request.POST['working_or_not']
+        userid = request.POST.get('userid')
+        usernname = request.POST.get('username')
+        gender = request.POST.get('gender')
+        age = request.POST.get('age')
+        email = request.POST.get('email')
+        phone_no = request.POST.get('phone_no')
+        working_or_not = request.POST.get('working_or_not')
         print(userid,usernname)
-        #new_user = userinfo(userid=userid,username=usernname,gender=gender,age=age,email=email,phone_no=phone_no,working_or_not=working_or_not)
-        #new_user.save()
+        new_user = userinfo(user_id=userid,username=usernname,gender=gender,age=age,email=email,phone_no=phone_no,working_or_not=working_or_not)
+        new_user.save()
         print("data written")
         
     return render(request,'userinfo.html')
