ddc5cebacc163def1ce51b040ec2cbf243edea9f
ShaluSenthilnathan <1RN20CS137@RNSIT.onmicrosoft.com>
2022-12-31 19:19:17 +0530

    updates

--- EXPENSE_TRACKER_DJANGO_FILES/USERACCOUNTS/forms.py
+++ EXPENSE_TRACKER_DJANGO_FILES/USERACCOUNTS/forms.py
@@ -11,3 +11,4 @@ class IncomesourcesTask(forms.ModelForm):
     class Meta:
         model = Incomesources
         fields = ['userid','source_id','monthly_income','rental_income','intrest_amount','other_sources','total_cash','bank_balance','net_amount']
+        
--- EXPENSE_TRACKER_DJANGO_FILES/USERACCOUNTS/views.py
+++ EXPENSE_TRACKER_DJANGO_FILES/USERACCOUNTS/views.py
@@ -1,4 +1,3 @@
-import re
 from django.shortcuts import render,HttpResponse 
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
